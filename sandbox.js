c7f978aa36485447d856b3f1e9856a0bba4003c9
jrnazareno <55470576+jrnazareno@users.noreply.github.com>
2020-06-11 14:51:11 +0800

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,9 @@
 
 
-const mssg = document.querySelector('p');
-mssg.setAttribute('style', 'color:red');
-
+const title = document.querySelector('h1');
+title.style.margin = '50px';
+title.style.color = 'teal';
+title.style.fontSize = '60px';
 
 alert('Apparently, there is nothing that cannot happen today.  -- Mark Twain');
 
