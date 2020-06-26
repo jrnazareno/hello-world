85ce0ff22f97a4bb357554138eac5480d0be3c77
jrnazareno <55470576+jrnazareno@users.noreply.github.com>
2020-06-26 17:19:26 +0800

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,27 @@
-const items = document.querySelectorAll('li');
+// const items = document.querySelectorAll('li');
 
-items.forEach(item => {
-  item.addEventListener('click', (e) => {
-    e.target.style.textDecoration = 'line-through';
-  });
-});  
+// items.forEach(item => {
+//  item.addEventListener('click', (e) => {
+//    e.target.style.textDecoration = 'line-through';
+//  });
+//});  
+
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  const li = document.createElement('li');
+  li.textContent = 'something new to do';
+  // ul.appendChild(li);  
+  ul.prepend(li);
+});
+
+ul.addEventListener('click', e => {
+  // console.log('event in UL');
+  // console.log(e);
+  // console.log(e.target, e);
+  if(e.target.tagName === 'LI'){
+    e.target.remove();
+  }
+});
+
